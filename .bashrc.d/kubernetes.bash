68dae7fbf441d6c67d2af0aa7ea1c90806a7aa8b
comame <dev@comame.xyz>
2025-06-14 09:08:02 +0900

    高速

--- .bashrc.d/kubernetes.bash
+++ .bashrc.d/kubernetes.bash
@@ -6,3 +6,44 @@ get-deploy-pods() {
 }
 
 alias kc='kubectl'
+
+deploy() {
+    if [ ! -e "deploy.json" ]; then
+        echo "deploy.json not found." 1>&2
+        exit 1
+    fi
+
+    DEPLOY_NAMESPACE=$(cat deploy.json | jq -r ".namespace")
+    DEPLOY_NAME=$(cat deploy.json | jq -r ".name")
+
+    if [ "$DEPLOY_NAMESPACE" = "null" ]; then
+        echo "Field \"namespace\" is required." 1>&2
+        return
+    fi
+
+    if [ "$DEPLOY_NAME" = "null" ]; then
+        echo "Field \"name\" is required." 1>&2
+        return
+    fi
+
+    DEPLOY_DOCKER_REGISTRY="registry.comame.dev"
+    DATE=$(date +%Y%m%d-%H%M%S)
+
+    PREBUILD=$(cat deploy.json | jq -r ".prebuild")
+    echo $PREBUILD
+    if [ ! "$PREBUILD" = "null" ]; then
+        echo "$PREBUILD"
+        eval "$PREBUILD"
+
+        if [ $? -ne 0 ]; then
+            return
+        fi
+    fi
+
+    eval "docker build -t $DEPLOY_DOCKER_REGISTRY/$DEPLOY_NAME:$DATE ."
+    eval "docker tag $DEPLOY_DOCKER_REGISTRY/$DEPLOY_NAME:$DATE $DEPLOY_DOCKER_REGISTRY/$DEPLOY_NAME:latest"
+    eval "docker push $DEPLOY_DOCKER_REGISTRY/$DEPLOY_NAME:$DATE"
+    eval "docker push $DEPLOY_DOCKER_REGISTRY/$DEPLOY_NAME:latest"
+
+    eval "kubectl rollout restart deploy/$DEPLOY_NAME -n $DEPLOY_NAMESPACE"
+}
---
+++ .bashrc.d/xenv.bash
@@ -0,0 +1,35 @@
+# Node.js
+
+__load-nvm() {
+    unset -f __load-nvm
+    unset -f nvm
+    unset -f npm
+    unset -f npx
+    unset -f node
+
+    . "$HOME/.nvm/nvm.sh"
+}
+
+nvm() { __load-nvm; nvm "$@"; }
+npm() { __load-nvm; npm "$@"; }
+npx() { __load-nvm; npx "$@"; }
+node() { __load-nvm; node "$@"; }
+
+# Ruby
+
+__load-rbenv() {
+    unset -f __load-rbenv
+    unset -f rbenv
+    unset -f gem
+    unset -f bundle
+    unset -f ruby
+    unset -f irb
+
+    eval "$(rbenv init -)"
+}
+
+rbenv() { __load-rbenv; rbenv "$@"; }
+gem() { __load-rbenv; gem "$@"; }
+bundle() { __load-rbenv; bundle "$@"; }
+ruby() { __load-rbenv; ruby "$@"; }
+irb() { __load-rbenv; irb "$@"; }
