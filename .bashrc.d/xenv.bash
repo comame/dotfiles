94648e0d2bd4864c0274ed0d748bf5de89db5612
comame <dev@comame.xyz>
2025-06-17 22:10:36 +0900

    xenv guard

--- .bashrc.d/xenv.bash
+++ .bashrc.d/xenv.bash
@@ -2,36 +2,40 @@
 
 # Node.js
 
-__load-nvm() {
-    unset -f __load-nvm
-    unset -f nvm
-    unset -f npm
-    unset -f npx
-    unset -f node
+if [ -e ~/.nvm/nvm.sh ]; then
+    __load-nvm() {
+        unset -f __load-nvm
+        unset -f nvm
+        unset -f npm
+        unset -f npx
+        unset -f node
 
-    . "$HOME/.nvm/nvm.sh"
-}
+        . "$HOME/.nvm/nvm.sh"
+    }
 
-nvm() { __load-nvm; nvm "$@"; }
-npm() { __load-nvm; npm "$@"; }
-npx() { __load-nvm; npx "$@"; }
-node() { __load-nvm; node "$@"; }
+    nvm() { __load-nvm; nvm "$@"; }
+    npm() { __load-nvm; npm "$@"; }
+    npx() { __load-nvm; npx "$@"; }
+    node() { __load-nvm; node "$@"; }
+fi
 
 # Ruby
 
-__load-rbenv() {
-    unset -f __load-rbenv
-    unset -f rbenv
-    unset -f gem
-    unset -f bundle
-    unset -f ruby
-    unset -f irb
+if command -v rbenv 1>/dev/null; then
+    __load-rbenv() {
+        unset -f __load-rbenv
+        unset -f rbenv
+        unset -f gem
+        unset -f bundle
+        unset -f ruby
+        unset -f irb
 
-    eval "$(rbenv init -)"
-}
+        eval "$(rbenv init -)"
+    }
 
-rbenv() { __load-rbenv; rbenv "$@"; }
-gem() { __load-rbenv; gem "$@"; }
-bundle() { __load-rbenv; bundle "$@"; }
-ruby() { __load-rbenv; ruby "$@"; }
-irb() { __load-rbenv; irb "$@"; }
+    rbenv() { __load-rbenv; rbenv "$@"; }
+    gem() { __load-rbenv; gem "$@"; }
+    bundle() { __load-rbenv; bundle "$@"; }
+    ruby() { __load-rbenv; ruby "$@"; }
+    irb() { __load-rbenv; irb "$@"; }
+fi
